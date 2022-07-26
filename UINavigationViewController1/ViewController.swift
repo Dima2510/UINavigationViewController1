a4b63a56b0238628ac552e269f38e354b15cfa34
Dima <protsack.dima25@gmail.com>
2022-07-27 00:53:11 +0300

    Initial Commit

---
+++ UINavigationViewController1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UINavigationViewController1
+//
+//  Created by Дмитрий Процак on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
